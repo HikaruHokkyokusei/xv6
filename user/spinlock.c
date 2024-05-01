4975d593be078b9673ecbdfe4f45f5a1bcfa7a71
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2024-04-30 21:12:21 -0400

    Modified `spinlock.c` to use mem fence instructions.

--- user/spinlock.c
+++ user/spinlock.c
@@ -7,18 +7,39 @@ u_lock_init(struct spinlock *lock, char *name)
 {
   lock->locked = 0;
   lock->name = name;
+  lock->cpu = (struct cpu *) -1;
+}
+
+int
+u_lock_holding(struct spinlock *lock)
+{
+  return (lock->locked && (((uint64) lock->cpu) == getpid()));
 }
 
 void
 u_lock_acquire(struct spinlock *lock)
 {
-  while(__sync_lock_test_and_set(&lock->locked, 1) != 0)
-    ;
+  if (u_lock_holding(lock) == 1) {
+    printf("Lock already acquired.");
+    exit(1);
+  }
+
+  while(__sync_lock_test_and_set(&lock->locked, 1) != 0);
+  __sync_synchronize();
+
+  lock->cpu = (struct cpu *) ((uint64) getpid());
 }
 
 void
-u_lock_release(struct spinlock * lock)
+u_lock_release(struct spinlock *lock)
 {
-  __sync_lock_test_and_set(&lock->locked, 0)
-    ;
+  if (u_lock_holding(lock) != 1) {
+    printf("Lock already released.");
+    exit(1);
+  }
+
+  lock->cpu = (struct cpu *) -1;
+
+  __sync_synchronize();
+  __sync_lock_test_and_set(&lock->locked, 0);
 }
